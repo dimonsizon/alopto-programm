c74f1d726c151bfa277d6c1d8e1e8f21ea91db8f
Dmitry Sizon <dimonsizon@mail.ru>
2016-03-05 13:48:54 +0300

    Удалил лишнее с продаж товаров

--- app/sale-products/sale-products.js
+++ app/sale-products/sale-products.js
@@ -16,7 +16,7 @@ angular.module('app.saleProducts', ['ngRoute'])
             'ourProfit': 0
         }
 
-
+        //получаем список моделей
         $http.get('https://script.google.com/macros/s/AKfycbx5xfsnqZ8ICQHQylIKKo1eABSvrVYIVMvZumVhGfvcJ02nfaus/exec').
             success(function (data, status) {
                 $scope.modelList = data.list.reverse();
@@ -68,10 +68,8 @@ angular.module('app.saleProducts', ['ngRoute'])
             });
         }
 
-        function calAllSaleData() {
-            
-            
-
+        function calAllSaleData() {           
+            //запись в таблицу продаж
             $.ajax({
                 url: 'https://script.google.com/macros/s/AKfycbxrRFcJhgMnNYj51ELQldNdIHzv3YRTJoaA1Dl9qA/exec',
                 data: JSON.stringify(saleModel),
@@ -86,26 +84,4 @@ angular.module('app.saleProducts', ['ngRoute'])
                 }
             });
         };
-
-
-        $scope.addNewItem = function () {
-            var model = {
-                    //"num": '888',
-                    "model": "test",
-                    "type": "red",
-                    "count": 100
-                };
-            $.ajax({
-                url: 'https://script.google.com/macros/s/AKfycbx5xfsnqZ8ICQHQylIKKo1eABSvrVYIVMvZumVhGfvcJ02nfaus/exec',
-                data: JSON.stringify(model),
-                dataType: "json",
-                type: "POST",
-                crossDomain: true,
-                success: function (data) {
-                    $scope.new.model = '';
-                    $scope.new.type = '';
-                    $scope.new.count = '';
-                }
-            });
-        }
     }]);
