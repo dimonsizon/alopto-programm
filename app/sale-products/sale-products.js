e889d015870add150730d0f4d4fa364391410b78
Dmitry Sizon <dimonsizon@mail.ru>
2016-03-05 20:57:51 +0300

    Added logic for get sale info.

--- app/sale-products/sale-products.js
+++ app/sale-products/sale-products.js
@@ -5,6 +5,7 @@ angular.module('app.saleProducts', ['ngRoute'])
 .controller('SaleProductsCtrl', ['$scope', '$http', '$rootScope',
     function ($scope, $http, $rootScope) {
         $scope.modelList = [];
+        $scope.currentSale = {};
         $scope.updateLoading = false;
         $scope.saleModel = {
             'date': Date.now(),
@@ -25,6 +26,75 @@ angular.module('app.saleProducts', ['ngRoute'])
                 }
             }).finally(function () {
             });
+
+        $scope.currentSale = {
+            getSaleInfo: function () {
+                $scope.currentSale.getCount();          //считаем количество               
+                $scope.currentSale.getPriceForEach();   //считаем цену для каждой позиции позиции
+                $scope.currentSale.getSumForEach();     //считаем сумму (руб)
+            },
+            getCount: function () {
+                var saleModel = $scope.saleModel;
+                for (var i = 0; i < $scope.modelList.length; i++) {
+
+                    $scope.modelList[i].saleCount == undefined ? $scope.modelList[i].saleCount = 0 : ''; //for disabled input
+
+                    //всего продали штук
+                    saleModel.allCaseCount = saleModel.allCaseCount + $scope.modelList[i].saleCount;
+
+                    //считаем сколько из них apple
+                    if ($scope.modelList[i].isApple) {
+                        saleModel.appleCaseCount = saleModel.appleCaseCount + $scope.modelList[i].saleCount;
+                    } else {    //считаем сколько из них других моделей
+                        saleModel.otherCaseCount = saleModel.otherCaseCount + $scope.modelList[i].saleCount;
+                    }                    
+                }
+            },
+            getPriceForEach: function () {
+                var saleModel = $scope.saleModel;
+                for (var i = 0; i < $scope.modelList.length; i++) {
+                    var item = $scope.modelList[i];
+                    switch (true) {
+                        case (saleModel.allCaseCount < 100):
+                            $scope.modelList[i].currentPrice = item.opt1; //текущая цена за единицу
+                            break;
+                        case (saleModel.allCaseCount >= 100 && saleModel.allCaseCount < 200):
+                            $scope.modelList[i].currentPrice = item.opt2;
+                            break;
+                        case (saleModel.allCaseCount >= 200 && saleModel.allCaseCount < 500):
+                            $scope.modelList[i].currentPrice = item.opt3;
+                            break;
+                        case (saleModel.allCaseCount >= 500 && saleModel.allCaseCount < 1000):
+                            $scope.modelList[i].currentPrice = item.opt4;
+                            break;
+                        case (saleModel.allCaseCount >= 1000 && saleModel.allCaseCount < 2000):
+                            $scope.modelList[i].currentPrice = item.opt5;
+                            break;
+                        case (saleModel.allCaseCount >= 2000 && saleModel.allCaseCount < 5000):
+                            $scope.modelList[i].currentPrice = item.opt6;
+                            break;
+                        case (saleModel.allCaseCount >= 5000 && saleModel.allCaseCount < 10000):
+                            $scope.modelList[i].currentPrice = item.opt7;
+                            break;
+                        case (saleModel.allCaseCount >= 10000):
+                            $scope.modelList[i].currentPrice = item.opt8;
+                            break;
+                    }
+                }                        
+            },
+            getSumForEach: function () {
+                for (var i = 0; i < $scope.modelList.length; i++) {
+                    //считаем сумму проданых
+                    if ($scope.modelList[i].saleCount > 0) { 
+                        $scope.saleModel.sum = $scope.saleModel.sum + ($scope.modelList[i].saleCount * $scope.modelList[i].currentPrice);
+
+                        //считаем чистую прибыль
+                        var ourProfit = ($scope.modelList[i].saleCount * $scope.modelList[i].currentPrice) - ($scope.modelList[i].saleCount * $scope.modelList[i].opt0); //продажа - закупка
+                        $scope.saleModel.ourProfit = $scope.saleModel.ourProfit + ourProfit;
+                    }
+                }
+            }
+        }
 
         $scope.updateList = function () {
             $scope.updateLoading = true;
@@ -51,30 +121,7 @@ angular.module('app.saleProducts', ['ngRoute'])
             });
         }
 
-        //вычисление текущей продажи
-        $scope.calcThisSale = function () {
-            var saleModel = $scope.saleModel;
-            for (var i = 0; i < $scope.modelList.length; i++) {
-
-                $scope.modelList[i].saleCount == undefined ? $scope.modelList[i].saleCount = 0 : ''; //for disabled input
-
-                //всего продали штук
-                saleModel.allCaseCount = saleModel.allCaseCount + $scope.modelList[i].saleCount;
-
-                //считаем сколько из них apple
-                if ($scope.modelList[i].isApple) {
-                    saleModel.appleCaseCount = saleModel.appleCaseCount + $scope.modelList[i].saleCount;
-                } else {    //считаем сколько из них других моделей
-                    saleModel.otherCaseCount = saleModel.otherCaseCount + $scope.modelList[i].saleCount;
-                }
-                //считаем сумму, на которую продали
-                //не готово еше =(
-
-
-
-                
-            }
-        }
+       
 
         function updateSalesTable() {           
             //запись в таблицу продаж
